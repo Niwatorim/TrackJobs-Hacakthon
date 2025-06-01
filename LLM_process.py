85a6d0318a52c741df4152c0c6e4351105a9a1eb
nauraalwa <naura21salwa@gmail.com>
2025-06-01 13:00:20 +0900

    main 2 has classes now and multiple object detection, LLM_process is now faster)

--- LLM_process.py
+++ LLM_process.py
@@ -5,20 +5,21 @@ LLM take input
 -system prompts: ur an emotional translator for blind people, and I need you to explain to blind people what is sent, which you will recieve the following content, of data and coordinates etc.
 -   user prompts: data object
 
-
 """
-from langchain_ollama import OllamaLLM
-from langchain_core.prompts import ChatPromptTemplate
 
-def LLM_proc(data,user):
-    template = """
-    You are assistance to blind people, and need to answer questions about the object. Respond with only 1 sentence. The details of the object is:
-    {data}
-    Here is the user request: {user}
-    """
-    model = OllamaLLM(model="gemma3")
-    prompt =  ChatPromptTemplate.from_template(template)
-    chain = prompt|model
-    result = chain.invoke({"data":data,"user":user})
-    print(result)
+def LLM_proc(data, user,chain):
+    if "where" in user.lower():
+        response = "Tell the location of the spoon only, in one sentence"
+    elif "cutlery" in user.lower():
+        response = "If any cutlery is found, say only that it exists and its direction"
+    else:
+        response = ""
+
+    result = chain.invoke({
+        "data": data,
+        "user": user,
+        "response": response
+    })
     return result
+
+
